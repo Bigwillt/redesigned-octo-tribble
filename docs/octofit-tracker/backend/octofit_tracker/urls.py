39d12682532fa5f5bba1571ebe99f7270853b75b
Tim W <Bigwillt@users.noreply.github.com>
2025-04-08 20:00:19 +0000

    2nd Reset

--- docs/octofit-tracker/backend/octofit_tracker/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-
-# Define API root and include routes for users, teams, activity, leaderboard, and workouts
-router = DefaultRouter()
-
-urlpatterns = [
-    path('', include(router.urls)),
-]
